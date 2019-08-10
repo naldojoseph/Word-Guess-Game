7bdfa4a24747b8c4ff02251a78d341c32d87ad04
Naldo Tumundo <naldo_j3@yahoo.com>
2019-08-09 23:43:23 -0700

    add function for var selectedwords

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,37 @@
 //Global Variables
 
+
+
 //arrays anad vars for holding data
-var wordOptions = ["Chris Evans", "Steve Carrel", "Chris Pratt", "Margot Robbie",];
+var wordOptions = ["chris", "steve", "pratt", "margot"];
 var selectedWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
+var blankAndSuccesses = [];
 var wrongLetters = [];
+
+// game counters
+
+var winCount = 0;
+var lossCount = 0;
+var guessLeft = 9;
+
+// FUNCTIONS (reusable blocks of code that i will call uopon when needed)
+// --------------------------------
+
+function startGame () {
+    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
+    
+
+    //testing/debugging
+
+    console.log(selectedWord);
+  
+}
+
+//main process 
+
+startGame();
+
+
+
