9a62cdd44988ad68b20d9a5bbd8b0d6bb59d374f
Naldo Tumundo <naldo_j3@yahoo.com>
2019-08-10 00:32:23 -0700

    created function, for loops, console logs, and game round condition with document.getelementbyid

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 
 
 //arrays anad vars for holding data
-var wordOptions = ["chris", "steve", "pratt", "margot"];
+var wordOptions = ["chrisevans", "stevecarrel", "cpratt", "margotrob"];
 var selectedWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
@@ -21,17 +21,48 @@ var guessLeft = 9;
 
 function startGame () {
     selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
-    
+    lettersinWord = selectedWord.split("");
+    numBlanks = lettersinWord.length;
+
+    // Reset
+    guessLeft = 9;
+    wrongLetters = [];
+    blankAndSuccesses = [];
+
+    // populate blanks and successes with right number of blanks
+    for (var i = 0; i < numBlanks; i++) {
+        blankAndSuccesses.push("_");
+    }
+
+    // change HTML to reflect game round condition
+  
+    document.getElementById("wordToGuess").innerHTML = blankAndSuccesses.join(" ");
+    document.getElementById("numGuesses").innerHTML = guessLeft;
+    document.getElementById("winCounter").innerHTML = winCount;
+    document.getElementById("lossCounter").innerHTML = lossCount;
 
     //testing/debugging
 
     console.log(selectedWord);
+    console.log(lettersinWord);
+    console.log(numBlanks);
+    console.log(blankAndSuccesses);
   
 }
 
+
+
 //main process 
 
+
+// initiate code the first time
 startGame();
 
+//register key clicks
+
+document.onkeyup = function (event) {
+    var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
+    alert(letterGuessed);
+}
 
 
