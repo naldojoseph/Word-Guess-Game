8e3f610214777c1d3766657c15bb61ea24be5e29
Naldo Tumundo <naldo_j3@yahoo.com>
2019-08-10 15:47:15 -0700

    finished up the function for hangman

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 
 
 //arrays anad vars for holding data
-var wordOptions = ["chrisevans", "stevecarrel", "cpratt", "margotrob"];
+var wordOptions = ["inception", "cars", "psycho", "coco", "avatar", "interstellar", "dunkirk"];
 var selectedWord = "";
 var lettersinWord = [];
 var numBlanks = 0;
@@ -41,17 +41,78 @@ function startGame () {
     document.getElementById("winCounter").innerHTML = winCount;
     document.getElementById("lossCounter").innerHTML = lossCount;
 
+
+
+
     //testing/debugging
 
     console.log(selectedWord);
     console.log(lettersinWord);
     console.log(numBlanks);
     console.log(blankAndSuccesses);
-  
 }
 
+function checkLetters(letter) {
+    // check if letter exists in code at all
+    var isLetterinWord = false;
+
+    for (var i = 0; i < numBlanks; i++){
+        if(selectedWord[i] == letter) {
+            isLetterinWord = true;
+
+        }
+    }
+    // check when in word letter exists, then populate out blankand successes aray
+    if(isLetterinWord) {
+        for (var i = 0; i < numBlanks; i++) {
+            if(selectedWord[i] == letter) {
+                blankAndSuccesses[i] = letter;
+            }
+        }
+    }
+    // letter wasnt found
+    else {
+        wrongLetters.push(letter);
+        guessLeft--
+    }
+
+    console.log(blankAndSuccesses);
+}
+
+function roundComplete () {
+    console.log("Win Count: " + winCount + " | Loss Count: " + lossCount + "| Guesses left " + guessLeft)
+     
+    //update HTMLto reflect the most recent count stats
+    document.getElementById("numGuesses").innerHTML = guessLeft;
+    document.getElementById("wordToGuess").innerHTML = blankAndSuccesses.join(" ");
+    document.getElementById("wrongGuesses").innerHTML = wrongLetters.join(" ");
 
 
+
+
+    //check to see if user won
+    if (lettersinWord.toString() == blankAndSuccesses.toString()) {
+        winCount++;
+        alert("Great Job! You've won $2");
+
+        //update the win counter in the HTML
+        document.getElementById("winCounter").innerHTML = winCount;
+
+        startGame()
+    }
+
+    //if the user lost
+    else if (guessLeft == 0) {
+        lossCount++;
+        alert("Booo! You lost! Try it again for $1?")
+        document.getElementById("lossCounter").innerHTML = lossCount;
+
+        startGame();
+    }
+
+
+
+}
 //main process 
 
 
@@ -60,9 +121,17 @@ startGame();
 
 //register key clicks
 
-document.onkeyup = function (event) {
+document.onkeyup = function(event) {
     var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
-    alert(letterGuessed);
+    checkLetters(letterGuessed);
+    roundComplete();
+
+
+    console.log(letterGuessed);
+   
+
+  
+
 }
 
 
