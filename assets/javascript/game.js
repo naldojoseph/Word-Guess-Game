365783a420380b51428842fe52b3227c11c292c6
Naldo Tumundo <naldo_j3@yahoo.com>
2019-08-09 22:35:11 -0700

    created some arrays and vars for javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,8 @@
+//Global Variables
+
+//arrays anad vars for holding data
+var wordOptions = ["Chris Evans", "Steve Carrel", "Chris Pratt", "Margot Robbie",];
+var selectedWord = "";
+var lettersinWord = [];
+var numBlanks = 0;
+var wrongLetters = [];
